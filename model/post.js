8764b85409c2c560de9280407bbf278ab0d6e683
damon <qingdoufd@163.com>
2015-03-13 13:26:06 +0800

    add post schema fields

--- model/post.js
+++ model/post.js
@@ -19,44 +19,66 @@ var mongoose = require('mongoose'),
 var formatDate = require('../lib/format');
 
 var post = new Schema({
+    //标题
     title: {
         type: String,
         required: true
     },
+    //类型？'post' or 'doc'
     type:{
         type:String,
         required: false,
         default: 'post'
     }
+    //内容
     , content: {
         type: String,
         required: true
     }
+    //简介
     , description: {
         type: String,
         required: true
     }
+    //标签
     , tags: {
         type: [String],
         required: false
     }
+    //发布者名字
     , name: {
         type: String
     }
+    //分类
     , category: {
         type: String,
-        required: true
+        required: false
     }
+    //创建时间
     , createtime: {
         type: Date,
         required: false,
         default: Date.now
     }
+    //更新时间
     , updatetime: {
         type: Date,
         required: false,
         default: Date.now
+    },
+    //是否支持评论
+    iscomment:{
+        type:Boolean,
+        require:true,
+        default:true
+    },
+    //状态? 0,草稿 1,已发布
+    status:{
+        type:Number,
+        required:false,
+        default :1
     }
+    //作者相关
     , author: {
         type: Schema.Types.ObjectId,
         ref: 'user'
