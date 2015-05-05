839da009b0716645eaa0cb2afa23ad092794fcf3
petitspois <qingdoufd@163.com>
2015-05-05 08:51:16 +0800

    error handling

--- server/index.js
+++ server/index.js
@@ -67,14 +67,14 @@ module.exports = function(){
 
 
     //Error Handling
-    //app.use(function* (next) {
-    //    try {
-    //        yield next;
-    //    } catch (err) {
-    //        console.log(err)
-    //        this.redirect('/404');
-    //    }
-    //});
+    app.use(function* (next) {
+        try {
+            yield next;
+        } catch (err) {
+            console.log(err)
+            this.redirect('/404');
+        }
+    });
 
     //routers
     router(app);
